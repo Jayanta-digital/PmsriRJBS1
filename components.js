e77ef7084e6432eeae1086e904b257176227e905
Jayanta Kumar Kakati <jayantakumarkakati1999@gmail.com>
2026-02-19 06:04:58 +0530

    Update components.js

--- components.js
+++ components.js
@@ -206,7 +206,7 @@ const Components = (() => {
                 <div class="footer__school-sub">${esc(cfg.district)}, ${esc(cfg.state)}</div>
               </div>
             </div>
-            <p class="footer__desc">A Government Lower Primary School serving children of Naharkatia since ${cfg.established}, under Samagra Shiksha Abhiyan.</p>
+            <p class="footer__desc">A Government Lower Primary School serving children of Radhakuchi Village,Kamrup District since ${cfg.established}, under Samagra Shiksha Abhiyan.</p>
             <div class="footer__social">
               <a href="${cfg.social.facebook}" aria-label="Facebook">
                 <svg viewBox="0 0 24 24" width="16" fill="currentColor"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
